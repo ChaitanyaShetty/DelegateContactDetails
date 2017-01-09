a7e1b252cdc3f14eae1a27f57a60520b1d67544e
test <test@tests-Mac.local>
2017-01-09 04:42:44 -0200

    Initial Commit

---
+++ DelegateContactDetailsTests/DelegateContactDetailsTests.m
@@ -0,0 +1,39 @@
+//
+//  DelegateContactDetailsTests.m
+//  DelegateContactDetailsTests
+//
+//  Created by test on 1/9/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DelegateContactDetailsTests : XCTestCase
+
+@end
+
+@implementation DelegateContactDetailsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
